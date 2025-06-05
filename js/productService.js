bb8474c19b397311c2ad84c35cf6a2f31deee220
keiner759vllalba <kjvillalbal@ufpso.edu.co>
2025-06-04 19:14:06 -0500

    Agregada la funcion de agregar un produto, recibe los datos que pide y los envia correctamente

--- js/productService.js
+++ js/productService.js
@@ -22,6 +22,7 @@ function getProducts() {
         .then(data => {
             if (data.status === 200) {
                 let listProducts = `
+                <button onclick="addProduct()" type="button" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></button>
                 <table class="table">
                     <thead>
                         <tr>
@@ -118,4 +119,82 @@ function showProductInfo(product) {
     document.getElementById('showModal').innerHTML = modalProduct
     const modal = new bootstrap.Modal(document.getElementById('modalProduct'))
     modal.show()
+}
+function addProduct() {
+    const modalUserAdd = `
+        <div class="modal fade" id="showModalProductAdd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Ver Usuario</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                    <div class="card-body">
+                    <form id="formAddProduct">
+                        <div class="row">
+                            <div class="col">
+                                <input type="text" id="nameProduct" class="form-control" placeholder="Nombre Producto" aria-label="First name" required>
+                            </div>
+                            <div class="col">
+                                <input type="number" id="precioProduct" class="form-control" placeholder="Precio" aria-label="Precio" required>
+                            </div>
+                        </div>
+
+                       
+
+                        <div class="row mt-3 ">
+                            <div class="col text-center">
+                                <button onclick="saveProduct()" type="button" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                            </div>
+                        </div> 
+                    </form>
+                </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    `;
+    document.getElementById('showModal').innerHTML = modalUserAdd;
+    const modal = new bootstrap.Modal(document.getElementById('showModalProductAdd'));
+    modal.show();
+}
+
+function saveProduct() {
+    const form = document.getElementById('formAddProduct');
+    if (form.checkValidity()) {
+        const productName = document.getElementById('nameProduct').value;
+        const cantidad = document.getElementById('precioProduct').value;
+        
+        
+        const product = {productName, cantidad};
+
+        const REQRES_ENDPOINT = 'https://fakestoreapi.com/products'
+        fetch(REQRES_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'x-api-key': 'reqres-free-v1'
+            },
+            body: JSON.stringify(product)
+        })
+        .then((response) =>response.json().then(data => ({status: response.status, info: data})))
+        .then(result => {
+            if (result.status === 200) {
+                document.getElementById('info').innerHTML = '<h3 class="text-success"><i class="fa-solid fa-check"></i> El Producto se guardo correctamente</h3>';
+            } else {
+                document.getElementById('info').innerHTML = '<h3><i class="fa-solid fa-x"></i> No se guardo el producto en la api</h3>';
+                const modalId = document.getElementById('showModalProductAdd');
+                const modal = bootstrap.Modal.getInstance(modalId);
+                modal.hide();
+            }
+        })
+    } else {
+        form.reportValidity();
+    }
 }
